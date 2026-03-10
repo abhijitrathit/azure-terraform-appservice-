d08084c178f71b0b929454dbbd764a43be6fede3
Abhijit Rath <108882191+abhijitrathit@users.noreply.github.com>
2026-03-10 15:38:02 +0530

    Add Azure resources for web app deployment

---
+++ min.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = var.service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = var.sku_name
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      java_version      = var.java_version
+      java_server       = "TOMCAT"
+      java_server_version = var.tomcat_version
+    }
+  }
+
+  app_settings = {
+    "CATALINA_OPTS" = var.catalina_opts
+  }
+}
